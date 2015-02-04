269d2abf276037a8d07ae3fb7ed4e54535a4eabc
Rachel VanBoening <vanboeningrr@gmail.com>
2015-02-04 08:53:43 -0600

    I think I'm close but I get a strange error msg.

--- MarvelApiTest.rb
+++ MarvelApiTest.rb
@@ -1,15 +1,18 @@
 require 'pry'
-
 require 'marvil_api'
 
+class MarvelApiTest
 
+  def initialize
+    @client = Marvel::Client.new
 
-
-@client = Marvel::Client.new
-
-@client.configure do |config|
-  config.api_key = '44748cba15b013ca8bc19b9c3f4a398a'
-  config.private_key = '28e4d05188bdcfec6a12b38ac5d49490bf3aaf2b'
+    @client.configure do |config|
+      config.api_key = '44748cba15b013ca8bc19b9c3f4a398a'
+      config.private_key = '28e4d05188bdcfec6a12b38ac5d49490bf3aaf2b'
+    end
+  end
 end
 
-API key: developer.marvel.com
+
+
+binding.pry
